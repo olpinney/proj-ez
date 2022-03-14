b98d487c49326e82fb7f8b86104f2711caaa1296
opinney <opinney@uchicago.edu>
2022-03-14 12:38:26 -0500

    fixed search terms

--- citizen_pull.py
+++ citizen_pull.py
@@ -6,6 +6,7 @@ proj-ez
 '''
 import sqlite3
 import os
+from unicodedata import category
 import pandas as pd
 import requests
 import util
@@ -25,26 +26,37 @@ import citizen_pull as c
 
 # to refresh run this:
 c.citizen_refresh() 
-#Notes: limit = int is optional. Estimate ~500 per day you want to update. 1000 is the max
+OR
+c.citizen_searches_refresh()
+#Notes: limit = int is optional. ~500 incidences happen per day, but max of 1000 can be pulled at once
 
 #to delete sql object and create a new one:
 c.reset_citizen(limit) #pick a 1000 limit 
 c.citizen_backfill('citizen') #to backfill with old data 
+OR
+c.citizen_searches_reset_and_backfill()
 
 #To see the results
-x=c.citizen_get_sql("citizen")
+x=c.citizen_get_sql(search="{desired search term}",table_name="citizen")
 
 #to pull from citizen 
 y=c.get_incidents_chicago(limit) #select limit= how many rows you want
 y=c.clean_citizen(y)
 
 
-#get citizen to work with the search terms
-#save into one SQL
-
 '''
 
-def citizen_refresh(limit=20,search=None,table_name=TABLE_NAME):
+SEARCH_CLEANED={None:None,'Assault':'Assault / Fight','Break_In':'Break In','Gun':'Gun Related','Harassment':'Harassment','Police':'Police Related','Pursuit':'Pursuit / Search','Theft':'Robbery / Theft','Weapon':'Weapon'}
+#this needs to be swapped 
+
+def add_cleaned_search(search,table_name):
+    if search is not None:
+        search=search.replace("/", "AND") #to avoid errors in writing to csv
+        search=search.replace(" ", "_") #to avoid errors in writing to sql
+        table_name+=f"_{search}"
+    return table_name
+
+def citizen_refresh(limit=100,search=None,table_name=TABLE_NAME):
     '''
     Pulls data since latest pull and updates csv and sql 
 
@@ -54,17 +66,23 @@ def citizen_refresh(limit=20,search=None,table_name=TABLE_NAME):
         limit (int): how many incidents to pull for first itteration
    
     ''' 
-    #customize per search term 
-    if search is not None:
-        table_name+=f"_{search}"
-
     #get data
     new_df=pull_recent(limit,search,table_name)
-    save_citizen_csv(new_df,table_name)
+    save_citizen_csv(new_df,search,table_name)
  
-    new_df_cleaned=clean_citizen(new_df) 
-    citizen_dedupe_SQL(new_df_cleaned,table_name)
+    new_df_cleaned=clean_citizen(new_df)
+    citizen_dedupe_SQL(new_df_cleaned,search,table_name)
 
+
+def citizen_searches_refresh(searches=SEARCH_CLEANED.keys()):
+    for search in searches:
+        citizen_refresh(search=search)
+
+def citizen_searches_reset_and_backfill(searches=SEARCH_CLEANED.keys()):
+    for search in searches:
+        reset_citizen(search=search)
+        citizen_backfill(search=search)
+        print(f"finished {search}")
 
 def reset_citizen(limit=1000,search=None,table_name=TABLE_NAME):
     '''
@@ -76,32 +94,34 @@ def reset_citizen(limit=1000,search=None,table_name=TABLE_NAME):
         limit (int): how many incidents to pull for first itteration
     
     '''
-     #customize per search term
-    if search is not None:
-        table_name+=f"_{search}"
+    table_name_w_search = add_cleaned_search(search,table_name)
 
-    util.drop_table(table_name)
+    util.drop_table(table_name_w_search)
     new_df=get_incidents_chicago(limit,search)
-    save_citizen_csv(new_df,table_name)
+    save_citizen_csv(new_df,search,table_name)
     
     new_df_cleaned=clean_citizen(new_df)
-    util.insert_sql(new_df_cleaned,table_name)
+    util.insert_sql(new_df_cleaned,table_name_w_search)
 
-def citizen_backfill(table_name,file_path="olpinney/data"):
+def citizen_backfill(search=None,table_name=TABLE_NAME,file_path="olpinney/data"):
     '''
     Back fills SQL table using csvs
     '''
-    sql_df=citizen_get_sql(table_name)
+    table_name_w_search = add_cleaned_search(search,table_name)
+
+    sql_df=citizen_get_sql(search,table_name)
 
     file_names=os.listdir(file_path)
 
     for file in file_names:
-        if re.match(f"^{table_name}_\d.*-.*\d\.csv$",file):
+        if re.match(f"^{table_name_w_search}_\d.*-.*\d\.csv$",file):
             with open(f"{file_path}/{file}",'r') as file:
                 df=pd.read_csv(file,mangle_dupe_cols=True)
                 
-
+            #make _geoloc back into list containing single dictionary 
             df["_geoloc"]=df["_geoloc"].apply(lambda x: [json.loads(x[1:-1].replace("'", "\""))])
+            #make categories into string from string represetnation of a single entry list
+            df["categories"]=df["_categories"].apply(lambda x: x[2:-2])
             df_clean=clean_citizen(df)
             sql_df=pd.concat([sql_df,df_clean])
                 
@@ -109,11 +129,11 @@ def citizen_backfill(table_name,file_path="olpinney/data"):
     print(sql_df)
 
     #save to sql
-    util.drop_table(table_name)
-    util.insert_sql(sql_df,table_name)            
+    util.drop_table(table_name_w_search)
+    util.insert_sql(sql_df,table_name_w_search)            
 
 
-def citizen_dedupe_SQL(new_df,table_name):
+def citizen_dedupe_SQL(new_df,search=None,table_name=TABLE_NAME):
     ''' 
     remove duplicated data between new_df and SQL and replace SQL
 
@@ -122,24 +142,19 @@ def citizen_dedupe_SQL(new_df,table_name):
         table_name (str): name of table in sql
 
     '''
+    table_name_w_search = add_cleaned_search(search,table_name)
+
     #combine the data 
-    old_df=citizen_get_sql(table_name)
+    old_df=citizen_get_sql(search,table_name)
 
     df=pd.concat([old_df,new_df])
-
-    print(old_df.columns)
-    print(old_df.dtypes)
-    print(new_df.columns)
-    print(new_df.dtypes)
-
-
     df.drop_duplicates(keep='first')
 
     #save to sql
-    util.drop_table(table_name)
-    util.insert_sql(new_df,table_name)
+    util.drop_table(table_name_w_search)
+    util.insert_sql(new_df,table_name_w_search)
 
-def citizen_get_sql(table_name):
+def citizen_get_sql(search=None,table_name=TABLE_NAME):
     ''' 
     get citizen data from SQL
 
@@ -149,11 +164,11 @@ def citizen_get_sql(table_name):
     Output:
         df (df): deduped df
     '''
-
+    table_name_w_search = add_cleaned_search(search,table_name)
     connection=util.get_connection()
     
     #pull in old data
-    query= f"select * from {table_name}" #specifically for the citizen table 
+    query= f"select * from {table_name_w_search}" #specifically for the citizen table 
     df=pd.read_sql(query, con = connection)
 
     df=df.drop(columns='index')
@@ -161,7 +176,7 @@ def citizen_get_sql(table_name):
     return df 
 
 
-def save_citizen_csv(new_df,table_name):
+def save_citizen_csv(new_df,search=None,table_name=TABLE_NAME):
     '''
     saves citizen to csv and sql
 
@@ -169,14 +184,15 @@ def save_citizen_csv(new_df,table_name):
         new_df (df): data to save
         table_name (str): name of table in csv and sql
     '''
+    table_name_w_search = add_cleaned_search(search,table_name)
 
     #create csv label 
-    max_new_date=max(new_df['created_at'])
-    min_new_date=min(new_df['created_at'])
+    max_new_date=convert_dt(max(new_df['created_at'])).date()
+    min_new_date=convert_dt(min(new_df['created_at'])).date()
 
     #save data 
     # updated util to create two files, new one from date and appends to existing citizen csv
-    util.df_to_csv(new_df,f"olpinney/data/{table_name}_{min_new_date}-{max_new_date}.csv")
+    util.df_to_csv(new_df,f"olpinney/data/{table_name_w_search}_{min_new_date}-{max_new_date}.csv")
 
 def clean_citizen(df):
     '''
@@ -184,10 +200,11 @@ def clean_citizen(df):
     '''
     df['lat']=df["_geoloc"].apply(lambda x: x[0]['lat']) 
     df['long']=df["_geoloc"].apply(lambda x: x[0]['lng']) 
-
+    df['categories']=df['categories'].apply(lambda x: ", ".join(x))
 
     to_drop=['_geoloc','updates','city_code','ranking.level','ranking.has_video','ranking.views','ranking.notifications','_highlightResult']
     df=df.drop(columns=to_drop)
+
     return df
 
 def pull_recent(limit,search,table_name):
@@ -204,9 +221,11 @@ def pull_recent(limit,search,table_name):
     '''
     c=util.get_cursor()
 
+    table_name_w_search = add_cleaned_search(search,table_name)
+
     #pull in old data
     try:
-        query= f"select created_at from {table_name}" #specifically for the citizen table 
+        query= f"select created_at from {table_name_w_search}" #specifically for the citizen table 
         old_dates=c.execute(query).fetchall()
         old_dates_cleaned=[int(date[0]) for date in old_dates]
         max_old_date=max(old_dates_cleaned)
@@ -272,7 +291,10 @@ def get_incidents(lat_long,limit,search=None):
     params.append(limit)
     if search:
         api_url+="&q={}&"
-        params.append(search)
+        try:
+            params.append(SEARCH_CLEANED[search])
+        except KeyError:
+            params.append(search)
         
     url=api_url.format(*params)
     request=requests.get(url) #add max-age cache-control for refreshes
